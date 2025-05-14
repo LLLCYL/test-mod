10f3017b3fb07c9508a4547f101c38cbb4b354cc
LLLCYL <161202079+LLLCYL@users.noreply.github.com>
2025-05-14 19:10:14 +0800

    counter block entity

--- src/main/java/lalawr/block/CounterBlock.java
+++ src/main/java/lalawr/block/CounterBlock.java
@@ -2,11 +2,15 @@ package lalawr.block;
 
 import com.mojang.serialization.MapCodec;
 import lalawr.block.entity.CounterBlockEntity;
-import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class CounterBlock extends BlockWithEntity {
 
@@ -25,7 +29,13 @@ public class CounterBlock extends BlockWithEntity {
     }
 
     @Override
-    protected BlockRenderType getRenderType(BlockState state) {
-        return BlockRenderType.MODEL;
+    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        if (world.getBlockEntity(pos) instanceof CounterBlockEntity counterBlockEntity) {
+            if (!world.isClient) {
+                counterBlockEntity.incrementCounter();
+                player.sendMessage(Text.literal("clicked %dth time".formatted(counterBlockEntity.getCounter())), true);
+            }
+            return ActionResult.SUCCESS;
+        } else return ActionResult.PASS;
     }
 }
--- src/main/java/lalawr/block/entity/CounterBlockEntity.java
+++ src/main/java/lalawr/block/entity/CounterBlockEntity.java
@@ -5,10 +5,21 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.util.math.BlockPos;
 
 public class CounterBlockEntity extends BlockEntity {
-    private int counter;
+    private int counter = 0;
 
     public CounterBlockEntity(BlockPos pos, BlockState state) {
         super(TestModBlockEntityTypes.COUNTER_BLOCK, pos, state);
     }
 
+    public void incrementCounter() {
+        counter++;
+    }
+
+    public void resetCounter() {
+        counter = 0;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
 }
